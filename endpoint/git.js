b642e9fb21e6547b2bf96048b7a1060140ad09a2
Christopher Dobler <chrisdobler@gmail.com>
2017-07-29 17:41:58 -0700

    Updates to git methods

--- endpoint/git.js
+++ endpoint/git.js
@@ -2,21 +2,35 @@ const spawn = require('child_process').spawnSync;
 
 const GIT_DIR = '--git-dir=.nebugit';
 const WORK_TREE = '--work-tree=.';
+const out = process.stdout;
+const outErr = process.stderr;
 
 const git = {
-  init: () => { return handler(spawn('git', [GIT_DIR, WORK_TREE, 'init'])); },
-  status: () => { return handler(spawn('git', [GIT_DIR, WORK_TREE, 'status']), false, false); },
-  commit: () => { return handler(spawn('git', [GIT_DIR, WORK_TREE, 'commit', '-m', 'date'])); },
-  push: () => { return handler(spawn('git', [GIT_DIR, WORK_TREE, 'push'])); },
+  init: () => { 
+    out.write('Initializing endpoint storage... '); 
+    return handler(spawn('git', [GIT_DIR, WORK_TREE, 'init']), false); 
+  },
+  status: () => { 
+    out.write('Checking Storage... '); 
+    return handler(spawn('git', [GIT_DIR, WORK_TREE, 'status']), false, false); },
+  stage: () => { 
+    out.write('Preparing Stage... '); 
+    return handler(spawn('git', [GIT_DIR, WORK_TREE, 'add', '.']), false); 
+  },
+  commit: () => { 
+    out.write('Saving Changes... '); 
+    return handler(spawn('git', [GIT_DIR, WORK_TREE, 'commit', '-m', 'date']), false); 
+  },
+  push: () => { 
+    return handler(spawn('git', [GIT_DIR, WORK_TREE, 'push'])); 
+  },
 };
 
 const handler = (proc, verbose = true, errors = true) => {
-  const out = process.stdout;
-  const outErr = process.stderr;
   if (proc.stderr.length < 1) {
-    verbose ? out.write(' [DONE]\n'.magenta) : out.write(' [DONE]\n'.magenta + proc.stdout);
+    verbose ? out.write(` ${'[DONE]\n'.magenta} ${proc.stdout.toString().red}`) : out.write(' [DONE]\n'.magenta);
   } else {
-    if(errors) outErr.write(`${'!!!NEBULIS ERROR!!!'.red} ${proc.stderr}`);
+    if(errors) outErr.write(`${'[ERROR]'.red} output: "${proc.stderr}"`);
   }
   return proc;
 }
