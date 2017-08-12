c1be30e02deef8b9aef66d164a2faaea201f3cd8
Christopher Dobler <chrisdobler@gmail.com>
2017-08-12 14:04:27 -0700

    git config extraction update

--- src/connect.js
+++ src/connect.js
@@ -38,11 +38,11 @@ const connect = {
 
     // write data to request body
     let str = fs.readFileSync('./.git/config').toString();
-    str = str.substring(str.indexOf('git@github.com:')+15, str.indexOf('.git'))
+    str = str.substring(str.indexOf('github.com:')+11, str.indexOf('.git'))
     const gitConfig = str.split('/');
 
-    config.owner = gitConfig[0];
-    config.project = gitConfig[1],
+    config.owner = gitConfig[3];
+    config.project = gitConfig[4],
 
     req.write(JSON.stringify({
       owner: config.owner,
